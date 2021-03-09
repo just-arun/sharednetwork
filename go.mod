f45df4debb4947fa35a007449e29186c7a87107c
just-arun <arunberry47@gmail.com>
2021-03-09 13:24:04 +0530

    adding static api and html file

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/just-arun/sharednetwork
+
+go 1.16
+
+require golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
--- main.go
+++ main.go
@@ -1,9 +1,150 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"net/http"
+	pathP "path"
+	"regexp"
+	"text/template"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
+var (
+	name     = 12032
+	pwd      = "supersecret"
+	path     = ""
+	port     string
+	fileList []*File
+)
+
+// File type
+type File struct {
+	Name   string `json:"name"`
+	Folder bool   `json:"folder"`
+	Image  bool   `json:"image"`
+	Video  bool   `json:"video"`
+	Size   int    `json:"size"`
+}
+
+func authServer(cb func()) {
+	var (
+		uName int
+	)
+	fmt.Print("n: ")
+	fmt.Scan(&uName)
+	fmt.Print("w: ")
+	uPwd, err := terminal.ReadPassword(0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if !(name == uName && pwd == string(uPwd)) {
+		log.Fatal("[failed]")
+		return
+	}
+	cb()
+}
+
+func setupStatic() {
+	fmt.Print("[path]:")
+	fmt.Scan(&path)
+	fmt.Print("PORT:")
+	fmt.Scan(&port)
+	http.Handle("/", http.FileServer(http.Dir(path)))
+}
+
+func serverFile(w http.ResponseWriter, r *http.Request) {
+
+	fp := pathP.Join("templates", "index.html")
+	tmpl, err := template.ParseFiles(fp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, map[string]string{
+		"path": path,
+	}); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func createFile(f fs.FileInfo) *File {
+	var file File
+	file.Folder = f.IsDir()
+	file.Size = int(f.Size())
+	file.Name = f.Name()
+	file.Image = false
+	file.Video = false
+	isImage, err := regexp.MatchString("{.jpeg|.jpg|.png|.webp}", f.Name())
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		if isImage {
+			file.Image = true
+		}
+	}
+
+	isVideo, err := regexp.MatchString("{.mp4}", f.Name())
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		if isVideo {
+			file.Video = true
+		}
+	}
+	return &file
+}
+
+func populateItems(p string) {
+	files, err := ioutil.ReadDir(p)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, v := range files {
+		fileList = append(fileList, createFile(v))
+	}
+	fmt.Println(fileList)
+}
+
+func openCors(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers")
+}
+
+func getData(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	openCors(w)
+	param := r.URL.Query().Get("param")
+	if param != "" {
+		w.Write([]byte("no1"))
+		return
+	}
+	value := path + param
+	populateItems(value)
+	data, err := json.Marshal(&fileList)
+	if err == nil {
+		fmt.Println(err.Error())
+		w.Write([]byte("no2"))
+		return
+	}
+	w.Write(data)
+	return
+}
+
 func main() {
-	fmt.Println()
+	populateItems("/Users/arunv/Documents/projects")
+	authServer(func() {
+		http.HandleFunc("/dash", serverFile)
+		http.HandleFunc("/api", getData)
+		setupStatic()
+		fmt.Println("server started")
+		http.ListenAndServe(port, nil)
+	})
 }
